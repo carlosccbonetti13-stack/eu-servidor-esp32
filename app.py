338b336309c33d681c4aa84c12cb66d88893ae63
carlosccbonetti13-stack <carlosccbonetti13@gmail.com>
2026-05-23 18:13:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def monitorar_status():
             
         time.sleep(5)
 
-@app.route('/ping', methods=['POST'])
+@app.route('/', methods=['POST'], strict_slashes=False)
 def ping():
     global ULTIMO_SINAL, DISPOSITIVO_ALERTA_DISPARADO
     dados = request.json
